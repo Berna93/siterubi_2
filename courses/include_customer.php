54ca84feec1a4413d4e1b1a4476ae9b63537aeae
Bernardelli <matheuzmoraes@gmail.com>
2017-07-10 23:22:11 -0300

    Correcao na mensagem de inclusao/exclusao de cliente de curso

--- courses/include_customer.php
+++ courses/include_customer.php
@@ -15,7 +15,8 @@
     indexCourseCustomers('tbl_courses_id', $customer['tbl_courses_id']);
     $foundKey = array_search($customer['tbl_customers_name_var'],array_column($courseCustomers, 'tbl_customers_name_var'));
 
-    if(!$foundKey) {
+
+    if($foundKey===false) {
        addCourseCustomer($customer);
     } else {
        $_SESSION['message'] = 'Não foi possível realizar esta operação. Cliente já inserido neste curso.';
